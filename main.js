0c7ef56e53c56adaddafe8d9ae74ffd1c718e345
mariajossdeny <mariajossdeny@gmail.com>
2025-01-24 17:57:54 -0600

    logic of the play more DOM

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 console.log("hola carambola")
 
-/*
-const numberSecret = Math.floor(Math.random() * 100 + 1); 
+const numberPlay = document.querySelectorAll('.number');
+const numberSecret = Math.floor(Math.random() * 30 + 1); //Funcion del numero secreto.
+const result = document.createElement("p");
+
+
+numberPlay.forEach((e, i) => {  //Funcion para recorrer cada opcion del juego. 
+    //console.log(e)
+    e.addEventListener('click', () =>{
+        console.log('lo hizo')
+        const clickNumber = `${i+1}`;
+        //console.log(`${i+1}`)
+
+        if(numberSecret.toString()===clickNumber.toString()){
+            result.textContent = "🎉🎉🎉Ohh Congratulations Won 🥳🥳🥳"
+            document.getElementById("results").appendChild(result)
+            //console.log('ohh ganaste Felicidades!!!')
+        } else {
+            result.textContent = "😱😱😱Sorry this is not the number ☹️☹️☹️"
+            document.getElementById("results").appendChild(result)
+           // console.log('Lo Siento ese no es el numero :( ')
+        } 
+    })  
+});
+
+
 
 console.log(numberSecret)
 
-const numberPlayer = parseInt(prompt('Adivina el numero del 1 al 100'));
 
-console.log(`Este es tu numero ${numberPlayer}`)
-
-if(numberSecret===numberPlayer){
-    console.log('ohh ganaste Felicidades!!!')
-} else {
-    console.log('Lo Siento ese no es el numero :( ')
-} */
